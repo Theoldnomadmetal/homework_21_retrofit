4777ed3681a9ddb0a6b0d85aa70830a9048eb120
Theoldnomadmetal <Deadharmonic@gmail.com>
2020-09-03 19:50:17 +0600

    initial commit

--- app/src/main/java/com/example/homework_20_retrofit/data/WeatherService.kt
+++ app/src/main/java/com/example/homework_20_retrofit/data/WeatherService.kt
@@ -9,10 +9,7 @@ interface WeatherService {
 
 
     @GET("data/2.5/weather")
-    fun getWeather(@Query("q") city:String,@Query("appid")appId :String ): Call<CurrentWeather>
+    fun getWeather(@Query("q") city:String,
+                   @Query("appId")appId :String ): Call<CurrentWeather>
 
 }
-
-
-
-//?q=Bishkek&appid=b11dc0a5c791b9901aa35dd046338dda
--- app/src/main/java/com/example/homework_20_retrofit/ui/MainActivity.kt
+++ app/src/main/java/com/example/homework_20_retrofit/ui/MainActivity.kt
@@ -10,8 +10,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.time.milliseconds
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,17 +22,45 @@ class MainActivity : AppCompatActivity() {
         RetrofitBuilder.getService()
             ?.getWeather("Bishkek", getString(R.string.api_key))
             ?.enqueue(object : Callback<CurrentWeather>{
+
                 override fun onFailure(call: Call<CurrentWeather>, t: Throwable) {
                     Log.d("asd","Fail")
                 }
-
                 override fun onResponse(
                     call: Call<CurrentWeather>,
                     response: Response<CurrentWeather>
                 ) {
-                    Log.d("asd","Ok")
-                }
+                    val data = response.body()
 
+                    val country = data?.sys?.country.toString()
+                    location.text = country
+
+                    val temp = data?.main?.temp?.minus(273)?.toInt().toString() +"°"
+                    temp_num.text = temp
+
+                    val maxTemp = data?.main?.temp_max?.minus(273)?.toInt().toString() +"°"
+                    temp_today_number.text = maxTemp
+
+                    val minTemp = data?.main?.temp_min?.minus(273)?.toInt().toString() +"°"
+                    min_number.text = minTemp
+
+                    val wind = data?.wind?.speed.toString() + " m/s"
+                    wind_num.text = wind
+
+                    val pressure = data?.main?.pressure.toString() + " mb"
+                    pressure_num.text = pressure
+
+                    val humidity = data?.main?.humidity.toString() + "%"
+                    humidity_num.text = humidity
+
+                    val cloudiness = data?.clouds?.all.toString() + "%"
+                    cloudiness_num.text = cloudiness
+
+                }
             })
+
     }
-}
+
+    }
+
+
